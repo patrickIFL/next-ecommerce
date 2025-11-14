5cf1b1e8cd4b0e96f7fcf679313051e74774f22c
Patrick Royce Perez <patrickperez.iformatlogic@gmail.com>
2025-11-14 13:07:09 +0800

    Added a few components from homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+import FeaturedProduct from "@/components/FeaturedProduct";
+import Footer from "@/components/Footer";
 import HeaderSlider from "@/components/HeaderSlider";
 import HomeProducts from "@/components/HomeProducts";
 import NavBar from "@/components/NavBar";
@@ -5,11 +7,16 @@ import NavBar from "@/components/NavBar";
 export default function Home() {
   return (
     <>
+    {/* To Fix: Responsiveness */}
+
       <NavBar />
       <div className="px-6 md:px-16 lg:px-32">
         <HeaderSlider />
         <HomeProducts />
+        <FeaturedProduct />
       </div>
+      <Footer />
+
     </>
   );
 }
