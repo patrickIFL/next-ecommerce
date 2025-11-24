538014fd73be1c7cc7704c743e64991f1f7cc2a6
Patrick Royce Perez <patrickperez.iformatlogic@gmail.com>
2025-11-24 08:50:35 +0800

    dummy data typing error fix

--- assets/assets.js
+++ assets/assets.js
@@ -135,7 +135,7 @@ export const HomeIcon = () => (
 
 export const productsDummyData = [
   {
-    "_id": "67a1f4e43f34a77b6dde9144",
+    "id": "67a1f4e43f34a77b6dde9144",
     "userId": "user_2sZFHS1UIIysJyDVzCpQhUhTIhw",
     "name": "Apple AirPods Pro 2nd gen",
     "description": "Apple AirPods Pro (2nd Gen) with MagSafe Case (USB-C) provide excellent sound, active noise cancellation, and a comfortable fit. The USB-C case ensures quick charging, and they pair seamlessly with Apple devices for an effortless audio experience.",
@@ -153,7 +153,7 @@ export const productsDummyData = [
     "__v": 0
   },
   {
-    "_id": "67a1f52e3f34a77b6dde914a",
+    "id": "67a1f52e3f34a77b6dde914a",
     "userId": "user_2sZFHS1UIIysJyDVzCpQhUhTIhw",
     "name": "Bose QuietComfort 45",
     "description": "The Bose QuietComfort 45 headphones are engineered for exceptional sound quality and unparalleled noise cancellation. With a 24-hour battery life and comfortable, lightweight design, these headphones deliver premium audio for any environment. Whether on a flight, in the office, or at home, the Bose QC45 blocks out distractions, offering an immersive listening experience.",
@@ -167,7 +167,7 @@ export const productsDummyData = [
     "__v": 0
   },
   {
-    "_id": "67a1f5663f34a77b6dde914c",
+    "id": "67a1f5663f34a77b6dde914c",
     "userId": "user_2sZFHS1UIIysJyDVzCpQhUhTIhw",
     "name": "Samsung Galaxy S23",
     "description": "The Samsung Galaxy S23 offers an all-encompassing mobile experience with its advanced AMOLED display, offering vibrant visuals and smooth interactions. Equipped with top-of-the-line fitness tracking features and cutting-edge technology, this phone delivers outstanding performance. With powerful hardware, a sleek design, and long battery life, the S23 is perfect for those who demand the best in mobile innovation.",
@@ -181,7 +181,7 @@ export const productsDummyData = [
     "__v": 0
   },
   {
-    "_id": "67a1f5993f34a77b6dde914e",
+    "id": "67a1f5993f34a77b6dde914e",
     "userId": "user_2sZFHS1UIIysJyDVzCpQhUhTIhw",
     "name": "Garmin Venu 2",
     "description": "The Garmin Venu 2 smartwatch blends advanced fitness tracking with sophisticated design, offering a wealth of features such as heart rate monitoring, GPS, and sleep tracking. Built with a 24-hour battery life, this watch is ideal for fitness enthusiasts and anyone looking to enhance their daily lifestyle. With a stunning AMOLED display and customizable watch faces, the Venu 2 combines technology with style seamlessly.",
@@ -195,7 +195,7 @@ export const productsDummyData = [
     "__v": 0
   },
   {
-    "_id": "67a1f5ef3f34a77b6dde9150",
+    "id": "67a1f5ef3f34a77b6dde9150",
     "userId": "user_2sZFHS1UIIysJyDVzCpQhUhTIhw",
     "name": "PlayStation 5",
     "description": "The PlayStation 5 takes gaming to the next level with ultra-HD graphics, a powerful 825GB SSD, and ray tracing technology for realistic visuals. Whether you're into high-action games or immersive storytelling, the PS5 delivers fast loading times, seamless gameplay, and stunning visuals. It's a must-have for any serious gamer looking for the ultimate gaming experience.",
@@ -209,7 +209,7 @@ export const productsDummyData = [
     "__v": 0
   },
   {
-    "_id": "67a1f70c3f34a77b6dde9156",
+    "id": "67a1f70c3f34a77b6dde9156",
     "userId": "user_2sZFHS1UIIysJyDVzCpQhUhTIhw",
     "name": "Canon EOS R5",
     "description": "The Canon EOS R5 is a game-changing mirrorless camera with a 45MP full-frame sensor, offering ultra-high resolution and the ability to shoot 8K video. Whether you're capturing professional-quality stills or cinematic video footage, this camera delivers exceptional clarity, speed, and color accuracy. With advanced autofocus and in-body stabilization, the R5 is ideal for photographers and videographers alike.",
@@ -223,7 +223,7 @@ export const productsDummyData = [
     "__v": 0
   },
   {
-    "_id": "67a1f7c93f34a77b6dde915a",
+    "id": "67a1f7c93f34a77b6dde915a",
     "userId": "user_2sZFHS1UIIysJyDVzCpQhUhTIhw",
     "name": "MacBook Pro 16",
     "description": "The MacBook Pro 16, powered by Apple's M2 Pro chip, offers outstanding performance with 16GB RAM and a 512GB SSD. Whether you're editing high-resolution video, developing software, or multitasking with ease, this laptop can handle the toughest tasks. It features a stunning Retina display with True Tone technology, making it a top choice for professionals in creative industries or anyone who demands premium performance in a portable form.",
@@ -237,7 +237,7 @@ export const productsDummyData = [
     "__v": 0
   },
   {
-    "_id": "67a1f8363f34a77b6dde915c",
+    "id": "67a1f8363f34a77b6dde915c",
     "userId": "user_2sZFHS1UIIysJyDVzCpQhUhTIhw",
     "name": "Sony WF-1000XM5",
     "description": "Sony WF-1000XM5 true wireless earbuds deliver immersive sound with Hi-Res Audio and advanced noise cancellation technology. Designed for comfort and quality, they provide a stable, snug fit for a secure listening experience. Whether you're working out or traveling, these earbuds will keep you connected with the world around you while enjoying rich, clear sound.",
@@ -251,7 +251,7 @@ export const productsDummyData = [
     "__v": 0
   },
   {
-    "_id": "67a1f85e3f34a77b6dde915e",
+    "id": "67a1f85e3f34a77b6dde915e",
     "userId": "user_2sZFHS1UIIysJyDVzCpQhUhTIhw",
     "name": "Samsung Projector 4k",
     "description": "The Samsung 4K Projector offers an immersive cinematic experience with ultra-high-definition visuals and realistic color accuracy. Equipped with a built-in speaker, it delivers rich sound quality to complement its stunning 4K resolution. Perfect for movie nights, gaming, or presentations, this projector is the ultimate choice for creating an at-home theater experience or professional setting.",
@@ -265,7 +265,7 @@ export const productsDummyData = [
     "__v": 0
   },
   {
-    "_id": "67a1fa4b3f34a77b6dde9166",
+    "id": "67a1fa4b3f34a77b6dde9166",
     "userId": "user_2sZFHS1UIIysJyDVzCpQhUhTIhw",
     "name": "ASUS ROG Zephyrus G16",
     "description": "The ASUS ROG Zephyrus G16 gaming laptop is powered by the Intel Core i9 processor and features an RTX 4070 GPU, delivering top-tier gaming and performance. With 16GB of RAM and a 1TB SSD, this laptop is designed for gamers who demand extreme power, speed, and storage. Equipped with a stunning 16-inch display, it's built to handle the most demanding titles and applications with ease.",
@@ -281,7 +281,7 @@ export const productsDummyData = [
 ]
 
 export const userDummyData = {
-  "_id": "user_2sZFHS1UIIysJyDVzCpQhUhTIhw",
+  "id": "user_2sZFHS1UIIysJyDVzCpQhUhTIhw",
   "name": "GreatStack",
   "email": "admin@example.com",
   "imageUrl": "https://img.clerk.com/eyJ0eXBlIjoiZGVmYXVsdCIsImlpZCI6Imluc18ycnlnUnFiUDBYT2dEZ2h1ZmRXcGlpdWV5OXoiLCJyaWQiOiJ1c2VyXzJzWkZIUzFVSUl5c0p5RFZ6Q3BRaFVoVElodyJ9",
@@ -293,12 +293,12 @@ export const userDummyData = {
 
 export const orderDummyData = [
   {
-    "_id": "67a20934b3db72db5cc77b2b",
+    "id": "67a20934b3db72db5cc77b2b",
     "userId": "user_2sZFHS1UIIysJyDVzCpQhUhTIhw",
     "items": [
       {
         "product": {
-          "_id": "67a1f4e43f34a77b6dde9144",
+          "id": "67a1f4e43f34a77b6dde9144",
           "userId": "user_2sZFHS1UIIysJyDVzCpQhUhTIhw",
           "name": "Apple AirPods Pro",
           "description": "Apple AirPods Pro (2nd Gen) with MagSafe Case (USB-C) provide excellent sound, active noise cancellation, and a comfortable fit. The USB-C case ensures quick charging, and they pair seamlessly with Apple devices for an effortless audio experience.",
@@ -315,12 +315,12 @@ export const orderDummyData = [
           "__v": 0
         },
         "quantity": 1,
-        "_id": "67a20934b3db72db5cc77b2c"
+        "id": "67a20934b3db72db5cc77b2c"
       }
     ],
     "amount": 406.99,
     "address": {
-      "_id": "67a1e4233f34a77b6dde9055",
+      "id": "67a1e4233f34a77b6dde9055",
       "userId": "user_2sZFHS1UIIysJyDVzCpQhUhTIhw",
       "fullName": "GreatStack",
       "phoneNumber": "0123456789",
@@ -335,12 +335,12 @@ export const orderDummyData = [
     "__v": 0
   },
   {
-    "_id": "67a20949b3db72db5cc77b2e",
+    "id": "67a20949b3db72db5cc77b2e",
     "userId": "user_2sZFHS1UIIysJyDVzCpQhUhTIhw",
     "items": [
       {
         "product": {
-          "_id": "67a1f52e3f34a77b6dde914a",
+          "id": "67a1f52e3f34a77b6dde914a",
           "userId": "user_2sZFHS1UIIysJyDVzCpQhUhTIhw",
           "name": "Bose QuietComfort 45",
           "description": "The Bose QuietComfort 45 headphones are engineered for exceptional sound quality and unparalleled noise cancellation. With a 24-hour battery life and comfortable, lightweight design, these headphones deliver premium audio for any environment. Whether on a flight, in the office, or at home, the Bose QC45 blocks out distractions, offering an immersive listening experience.",
@@ -354,12 +354,12 @@ export const orderDummyData = [
           "__v": 0
         },
         "quantity": 1,
-        "_id": "67a20949b3db72db5cc77b2f"
+        "id": "67a20949b3db72db5cc77b2f"
       }
     ],
     "amount": 335.99,
     "address": {
-      "_id": "67a1e4233f34a77b6dde9055",
+      "id": "67a1e4233f34a77b6dde9055",
       "userId": "user_2sZFHS1UIIysJyDVzCpQhUhTIhw",
       "fullName": "GreatStack",
       "phoneNumber": "0123456789",
@@ -374,12 +374,12 @@ export const orderDummyData = [
     "__v": 0
   },
   {
-    "_id": "67a209bab3db72db5cc77b34",
+    "id": "67a209bab3db72db5cc77b34",
     "userId": "user_2sZFHS1UIIysJyDVzCpQhUhTIhw",
     "items": [
       {
         "product": {
-          "_id": "67a1f4e43f34a77b6dde9144",
+          "id": "67a1f4e43f34a77b6dde9144",
           "userId": "user_2sZFHS1UIIysJyDVzCpQhUhTIhw",
           "name": "Apple AirPods Pro",
           "description": "Apple AirPods Pro (2nd Gen) with MagSafe Case (USB-C) provide excellent sound, active noise cancellation, and a comfortable fit. The USB-C case ensures quick charging, and they pair seamlessly with Apple devices for an effortless audio experience.",
@@ -396,12 +396,12 @@ export const orderDummyData = [
           "__v": 0
         },
         "quantity": 1,
-        "_id": "67a209bab3db72db5cc77b35"
+        "id": "67a209bab3db72db5cc77b35"
       }
     ],
     "amount": 406.99,
     "address": {
-      "_id": "67a1e4233f34a77b6dde9055",
+      "id": "67a1e4233f34a77b6dde9055",
       "userId": "user_2sZFHS1UIIysJyDVzCpQhUhTIhw",
       "fullName": "GreatStack",
       "phoneNumber": "0123456789",
@@ -416,12 +416,12 @@ export const orderDummyData = [
     "__v": 0
   },
   {
-    "_id": "67a209bab3db72db5cc77b34",
+    "id": "67a209bab3db72db5cc77b34",
     "userId": "user_2sZFHS1UIIysJyDVzCpQhUhTIhw",
     "items": [
       {
         "product": {
-          "_id": "67a1f4e43f34a77b6dde9144",
+          "id": "67a1f4e43f34a77b6dde9144",
           "userId": "user_2sZFHS1UIIysJyDVzCpQhUhTIhw",
           "name": "Apple AirPods Pro",
           "description": "Apple AirPods Pro (2nd Gen) with MagSafe Case (USB-C) provide excellent sound, active noise cancellation, and a comfortable fit. The USB-C case ensures quick charging, and they pair seamlessly with Apple devices for an effortless audio experience.",
@@ -438,12 +438,12 @@ export const orderDummyData = [
           "__v": 0
         },
         "quantity": 1,
-        "_id": "67a209bab3db72db5cc77b35"
+        "id": "67a209bab3db72db5cc77b35"
       }
     ],
     "amount": 406.99,
     "address": {
-      "_id": "67a1e4233f34a77b6dde9055",
+      "id": "67a1e4233f34a77b6dde9055",
       "userId": "user_2sZFHS1UIIysJyDVzCpQhUhTIhw",
       "fullName": "GreatStack",
       "phoneNumber": "0123456789",
@@ -458,12 +458,12 @@ export const orderDummyData = [
     "__v": 0
   },
   {
-    "_id": "67a209bab3db72db5cc77b34",
+    "id": "67a209bab3db72db5cc77b34",
     "userId": "user_2sZFHS1UIIysJyDVzCpQhUhTIhw",
     "items": [
       {
         "product": {
-          "_id": "67a1f4e43f34a77b6dde9144",
+          "id": "67a1f4e43f34a77b6dde9144",
           "userId": "user_2sZFHS1UIIysJyDVzCpQhUhTIhw",
           "name": "Apple AirPods Pro",
           "description": "Apple AirPods Pro (2nd Gen) with MagSafe Case (USB-C) provide excellent sound, active noise cancellation, and a comfortable fit. The USB-C case ensures quick charging, and they pair seamlessly with Apple devices for an effortless audio experience.",
@@ -480,12 +480,12 @@ export const orderDummyData = [
           "__v": 0
         },
         "quantity": 1,
-        "_id": "67a209bab3db72db5cc77b35"
+        "id": "67a209bab3db72db5cc77b35"
       }
     ],
     "amount": 406.99,
     "address": {
-      "_id": "67a1e4233f34a77b6dde9055",
+      "id": "67a1e4233f34a77b6dde9055",
       "userId": "user_2sZFHS1UIIysJyDVzCpQhUhTIhw",
       "fullName": "GreatStack",
       "phoneNumber": "0123456789",
@@ -500,12 +500,12 @@ export const orderDummyData = [
     "__v": 0
   },
   {
-    "_id": "67a209bab3db72db5cc77b34",
+    "id": "67a209bab3db72db5cc77b34",
     "userId": "user_2sZFHS1UIIysJyDVzCpQhUhTIhw",
     "items": [
       {
         "product": {
-          "_id": "67a1f4e43f34a77b6dde9144",
+          "id": "67a1f4e43f34a77b6dde9144",
           "userId": "user_2sZFHS1UIIysJyDVzCpQhUhTIhw",
           "name": "Apple AirPods Pro",
           "description": "Apple AirPods Pro (2nd Gen) with MagSafe Case (USB-C) provide excellent sound, active noise cancellation, and a comfortable fit. The USB-C case ensures quick charging, and they pair seamlessly with Apple devices for an effortless audio experience.",
@@ -522,12 +522,12 @@ export const orderDummyData = [
           "__v": 0
         },
         "quantity": 1,
-        "_id": "67a209bab3db72db5cc77b35"
+        "id": "67a209bab3db72db5cc77b35"
       }
     ],
     "amount": 406.99,
     "address": {
-      "_id": "67a1e4233f34a77b6dde9055",
+      "id": "67a1e4233f34a77b6dde9055",
       "userId": "user_2sZFHS1UIIysJyDVzCpQhUhTIhw",
       "fullName": "GreatStack",
       "phoneNumber": "0123456789",
@@ -542,12 +542,12 @@ export const orderDummyData = [
     "__v": 0
   },
   {
-    "_id": "67a209bab3db72db5cc77b34",
+    "id": "67a209bab3db72db5cc77b34",
     "userId": "user_2sZFHS1UIIysJyDVzCpQhUhTIhw",
     "items": [
       {
         "product": {
-          "_id": "67a1f4e43f34a77b6dde9144",
+          "id": "67a1f4e43f34a77b6dde9144",
           "userId": "user_2sZFHS1UIIysJyDVzCpQhUhTIhw",
           "name": "Apple AirPods Pro",
           "description": "Apple AirPods Pro (2nd Gen) with MagSafe Case (USB-C) provide excellent sound, active noise cancellation, and a comfortable fit. The USB-C case ensures quick charging, and they pair seamlessly with Apple devices for an effortless audio experience.",
@@ -564,12 +564,12 @@ export const orderDummyData = [
           "__v": 0
         },
         "quantity": 1,
-        "_id": "67a209bab3db72db5cc77b35"
+        "id": "67a209bab3db72db5cc77b35"
       }
     ],
     "amount": 406.99,
     "address": {
-      "_id": "67a1e4233f34a77b6dde9055",
+      "id": "67a1e4233f34a77b6dde9055",
       "userId": "user_2sZFHS1UIIysJyDVzCpQhUhTIhw",
       "fullName": "GreatStack",
       "phoneNumber": "0123456789",
@@ -587,7 +587,7 @@ export const orderDummyData = [
 
 export const addressDummyData = [
   {
-    "_id": "67a1e4233f34a77b6dde9055",
+    "id": "67a1e4233f34a77b6dde9055",
     "userId": "user_2sZFHS1UIIysJyDVzCpQhUhTIhw",
     "fullName": "GreatStack",
     "phoneNumber": "0123456789",
