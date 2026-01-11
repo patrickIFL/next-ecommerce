9ecd74c97847b63fe96dda332159bdf4ec7df1ee
Patrick Royce Perez <patrickperez0530@gmail.com>
2026-01-12 00:45:33 +0800

    [forced dynamic]

--- app/wishlist/products/page.tsx
+++ app/wishlist/products/page.tsx
@@ -1,41 +1,8 @@
-"use client";
+export const dynamic = "force-dynamic";
+import WishlistPage from "./WishlistPage"
 
-import { useSearchParams } from "next/navigation";
-import { Archive } from "lucide-react";
-
-import PaginatedProductGrid from "@/components/common/PaginatedProductGrid";
-
-import { useWishlistedProductsPage } from "@/hooks/FetchProduct/useWishlistedProducts";
-import ProductPageTitle from "@/components/common/ProductPageTitle";
-
-export default function WishlistPage() {
-  const searchParams = useSearchParams();
-
-  /* =========================
-     PAGE FROM URL
-  ========================= */
-  const raw = Number(searchParams.get("page"));
-  const page = Number.isInteger(raw) && raw > 0 ? raw : 1;
-
-  const { data, isLoading } = useWishlistedProductsPage(page);
-
-  const products = data?.products ?? [];
-  const pagination = data?.pagination;
-
-  return (
-    <div className="mt-16 flex flex-col items-start px-6 md:px-16 lg:px-32">
-      {/* HEADER */}
-      <ProductPageTitle title="My Wishlist" />
-
-      {/* PAGINATED PRODUCT GRID */}
-      <PaginatedProductGrid
-        products={products}
-        isLoading={isLoading}
-        pagination={pagination}
-        emptyIcon={Archive}
-        emptyTitle="No Products Found"
-        emptyDescription="Your wishlist is currently empty."
-      />
-    </div>
-  );
+function Page() {
+  return <WishlistPage />
 }
+
+export default Page
